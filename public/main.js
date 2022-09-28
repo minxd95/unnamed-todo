c8e8b80a0510996618bb7d2e64037812c4a003b2
minxd95 <newobj95@gmail.com>
2022-09-28 12:08:31 +0900

    electron-builder settings

--- public/main.js
+++ public/main.js
@@ -1,18 +1,25 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const isDev = require("electron-is-dev");
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    // webPreferences: {
-    //   preload: path.join(__dirname, "preload.js"),
-    // },
+    webPreferences: {
+      // preload: path.join(__dirname, "preload.js"),
+    },
   });
 
-  mainWindow.loadURL("http://localhost:3000");
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
 
-  mainWindow.webContents.openDevTools({ mode: "detach" });
+  if (isDev) {
+    mainWindow.webContents.openDevTools({ mode: "detach" });
+  }
 }
 
 app.whenReady().then(() => {
